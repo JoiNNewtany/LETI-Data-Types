844aaea7cd9f03cdea2d968105baf1c4e820d313
Solar Flare <34160814+JoiNNewtany@users.noreply.github.com>
2020-10-07 22:06:32 +0300

    Rewrite GetSize function
    GetSize is now called PrintSize and it prints the size of a variable directly to console and doesn't use sizeof().

--- LETI-Data-Types/include/LETI-Data-Types/data_types_helper.hpp
+++ LETI-Data-Types/include/LETI-Data-Types/data_types_helper.hpp
@@ -11,12 +11,20 @@ class DTHelper
 public:
 	~DTHelper() {}
 
+	// Returns a binary representation of a number
 	static std::string GetBinary(int num);
 	static std::string GetBinary(float num);
 	static std::string GetBinary(double num);
 
+	// Prints a size of a variable in bytes
 	template <typename T>
-	static int GetSize(T var) { return sizeof(var); }
+	static inline void PrintSize(T var)
+	{
+		T *ptr = 0;
+		// Pointer increments by the size of the type
+		ptr++;
+		printf("%d", ptr);
+	}
 
 private:
 	// Disallow creating an instance of this object
